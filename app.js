30b8cbec36958250fc832a52c21b4a9e290ee450
Deyan Vladkov <Deyan Vladkov>
2017-08-03 09:56:03 +0100

    database connection

--- app.js
+++ app.js
@@ -4,18 +4,44 @@ var express = require('express'),
 
 app.use(express.static('public'));
 
+// MongoDB
+var MongoClient = require('mongodb').MongoClient;
+var database;
+var url = 'mongodb://localhost:27017/image-search';
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function (err, db) {
+    if (!err) {
+        console.log('Database connection established');
+        database = db;
+    }
+});
+
+// Image search
 var GoogleImages = require('google-images');
 var credentials = require('./config/credentials');
-
 var client = new GoogleImages(credentials.cseId, credentials.apiKey);
 
-client.search('dogs')
-    .then(images => {        
-        console.log(images);
-    });
+// Sample image search
+//client.search('dogs')
+//    .then(images => {
+//        console.log(images);
+//    });
 
 app.get('/', function (req, res) {
     res.sendFile('index.html');
+});
+
+app.get('/api/latest', (req, res) => {
+    database.collection('log').find({},{ _id: 0 }).limit(10).toArray(function (err, result) {
+        if (err) throw err
+        //console.log(result)
+        res.json(result);
+    });
+});
+
+// Redirect to main page
+app.get('/api', function (req, res) {
+    res.redirect('/');
 });
 
 // Return 404 on missing pages
