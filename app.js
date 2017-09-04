5712b76b43912ea661e808a004eb2954358d9f62
Deyan Vladkov <deian.vladkov@gmail.com>
2017-09-04 09:35:31 +0100

    Log search term and time to MongoDB

--- app.js
+++ app.js
@@ -20,19 +20,59 @@ MongoClient.connect(url, function (err, db) {
 var GoogleImages = require('google-images');
 var credentials = require('./config/credentials');
 var client = new GoogleImages(credentials.cseId, credentials.apiKey);
+// Test
+var findImages = require('./lib/findimages');
 
-// Sample image search
-//client.search('dogs')
-//    .then(images => {
-//        console.log(images);
-//    });
+app.get('/test', function (req, res) {
+    console.log(findImages.find());
+    res.send(findImages.find());
+});
+
 
 app.get('/', function (req, res) {
     res.sendFile('index.html');
 });
 
+
+// Get timestamp before database recording: var timestamp = new Date().toISOString();
+
+// Example: http://localhost:8000/api/search?term=lake&offset=13
+app.get('/api/search', function (req, res) {
+    console.log(req.query) // should return the query string
+    var searchTerm = req.query.term;
+    console.log("Searching for: ", searchTerm);
+    var offset = req.query.offset | 0;
+    //console.log(req.query.term, req.query.offset);   
+
+    if (searchTerm) {        
+        client.search(searchTerm).then(images => {
+           // console.log(images);            
+            res.json(images);
+        });
+        
+        // log search term to database        
+        var now = new Date();
+        var date = now.toISOString();
+        database.collection('log').insert( { term: searchTerm, when: date } );
+
+    } else {
+        console.log('No search term')
+        res.send('No search term');
+    }
+
+    // console.log('Search for: ', searchTerm);
+    //console.log('Offset: ', offset);    
+    // var output = JSON.stringify(req.params);
+    // console.log("Term: " + JSON.stringify(req.params));
+    
+    //res.send(req.params);
+    //res.send('search ' + req.params.term + 'offset: ' + req.params.offset );    
+});
+
 app.get('/api/latest', (req, res) => {
-    database.collection('log').find({},{ _id: 0 }).limit(10).toArray(function (err, result) {
+    database.collection('log').find({}, {
+        _id: 0
+    }).limit(10).toArray(function (err, result) {
         if (err) throw err
         //console.log(result)
         res.json(result);
